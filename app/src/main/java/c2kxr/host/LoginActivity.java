4986ed10fac0b95797abac3d281c58449d9330f1
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-02-02 14:41:22 +0800

    Add Main Activity

--- app/src/main/java/c2kxr/host/LoginActivity.java
+++ app/src/main/java/c2kxr/host/LoginActivity.java
@@ -1,7 +1,9 @@
 package c2kxr.host;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
     }
 }
