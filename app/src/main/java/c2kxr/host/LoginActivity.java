0572b824b1f622d361b17dda886554e825b34792
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-02-02 17:35:44 +0800

    Link Main Activity to Dashboards
    MainActivity has buttons going to:
    - OrganizerDashboardActivity
    - ProviderDashboardActivity

--- app/src/main/java/c2kxr/host/LoginActivity.java
+++ app/src/main/java/c2kxr/host/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
         findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, OrganizerDashboardActivity.class));
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
 
