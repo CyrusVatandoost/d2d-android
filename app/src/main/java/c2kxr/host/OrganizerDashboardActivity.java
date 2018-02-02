34987a435e8987d3505fc589f244592bb6673afa
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-02-02 18:51:07 +0800

    Add Add Event Activity
    Added:
    - AddEventActivity
    - Buttons at OrganizerDashboardActivity

--- app/src/main/java/c2kxr/host/OrganizerDashboardActivity.java
+++ app/src/main/java/c2kxr/host/OrganizerDashboardActivity.java
@@ -1,7 +1,9 @@
 package c2kxr.host;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OrganizerDashboardActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class OrganizerDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer_dashboard);
+
+        // login button opens MainActivity
+        findViewById(R.id.btnAddEvent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OrganizerDashboardActivity.this, AddEventActivity.class));
+            }
+        });
+
     }
 }
