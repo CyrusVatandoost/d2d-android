92f821e263c63e0efe7eb7ca358fdce4ccd45e2c
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-02-02 17:30:22 +0800

    Added Empty Screens
    Added empty screens:
    - OrganizerDashboardActivity
    - ProviderDashboardActivity
    - RegistratrionActivity

---
+++ app/src/main/java/c2kxr/host/OrganizerDashboardActivity.java
@@ -0,0 +1,13 @@
+package c2kxr.host;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrganizerDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizer_dashboard);
+    }
+}
