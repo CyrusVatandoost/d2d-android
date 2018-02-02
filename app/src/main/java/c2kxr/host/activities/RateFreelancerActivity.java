12f33d94763f2edf5d565165f727182ad4edd705
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-02-03 04:17:45 +0800

    Fix Gradle Bug

--- app/src/main/java/c2kxr/host/activities/RateFreelancerActivity.java
+++ app/src/main/java/c2kxr/host/activities/RateFreelancerActivity.java
@@ -3,6 +3,8 @@ package c2kxr.host.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import c2kxr.host.R;
+
 public class RateFreelancerActivity extends AppCompatActivity {
 
     @Override
