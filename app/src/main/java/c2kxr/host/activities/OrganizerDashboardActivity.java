9d55fd258b5f07572c9bd90c9c6cb43992b27eb4
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-02-02 19:18:02 +0800

    Add OrganizerEventListActivity

--- app/src/main/java/c2kxr/host/activities/OrganizerDashboardActivity.java
+++ app/src/main/java/c2kxr/host/activities/OrganizerDashboardActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import c2kxr.host.OrganizerEventListActivity;
 import c2kxr.host.R;
 
 public class OrganizerDashboardActivity extends AppCompatActivity {
@@ -14,11 +15,19 @@ public class OrganizerDashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer_dashboard);
 
-        // login button opens MainActivity
+        // add event button opens MainActivity
         findViewById(R.id.btnAddEvent).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(OrganizerDashboardActivity.this, AddEventActivity.class));
+            }
+        });
+
+        // view organizer events button opens MainActivity
+        findViewById(R.id.btnViewEvents).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OrganizerDashboardActivity.this, OrganizerEventListActivity.class));
             }
         });
 
