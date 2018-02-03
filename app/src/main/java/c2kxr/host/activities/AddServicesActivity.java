9c2bd26ae4d0e2b33b2bd9def21300d326495db4
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-02-03 14:18:28 +0800

    Link AddServicesActivity to EventActivity

--- app/src/main/java/c2kxr/host/activities/AddServicesActivity.java
+++ app/src/main/java/c2kxr/host/activities/AddServicesActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -77,6 +78,13 @@ public class AddServicesActivity extends AppCompatActivity {
             }
         });
         recyclerViewBox.setAdapter(serviceBoxListAdapter);
+
+        findViewById(R.id.btnPostEvent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddServicesActivity.this, EventActivity.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/c2kxr/host/adapters/TemplateListAdapter.java
+++ app/src/main/java/c2kxr/host/adapters/TemplateListAdapter.java
@@ -21,7 +21,7 @@ import c2kxr.host.classes.Template;
 public class TemplateListAdapter extends RecyclerView.Adapter<TemplateListAdapter.TemplateListViewHolder> {
 
     public interface OnItemClickListener {
-        void onItemClick(Event item);
+        void onItemClick(Template item);
     }
 
     private ArrayList<Template> temps;
@@ -29,12 +29,12 @@ public class TemplateListAdapter extends RecyclerView.Adapter<TemplateListAdapte
     private Context context;
 
     public TemplateListAdapter(Context context, ArrayList<Template> temps, TemplateListAdapter.OnItemClickListener listener) {
-
         this.context = context;
         this.temps = temps;
         this.listener = listener;
     }
     @Override
+
     public TemplateListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_event_template,parent,false);
         TemplateListAdapter.TemplateListViewHolder holder = new TemplateListAdapter.TemplateListViewHolder(view);
@@ -46,7 +46,7 @@ public class TemplateListAdapter extends RecyclerView.Adapter<TemplateListAdapte
         Template template = temps.get(position);
         //holder.templateImage.setImageDrawable(template.getTemplateImage());
         holder.templateName.setText(template.getTemplateName());
-        holder.bind(template listener);
+        holder.bind(template, listener);
     }
 
     @Override
@@ -67,7 +67,7 @@ public class TemplateListAdapter extends RecyclerView.Adapter<TemplateListAdapte
             templateName =  itemView.findViewById(R.id.templateName);
         }
 
-        public void bind(final Event item, final TemplateListAdapter.OnItemClickListener listener) {
+        public void bind(final Template item, final TemplateListAdapter.OnItemClickListener listener) {
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
